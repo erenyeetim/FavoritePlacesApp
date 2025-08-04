170ec0926a04935f64124df0e64669a94c214b03
erenyeetim <eren.yeetim@gmail.com>
2025-08-04 21:05:50 +0300

    Ekranlar oluşturuldu.

---
+++ screens/Map.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+function Map() {
+  return (
+    <View>
+      <Text>Map</Text>
+    </View>
+  );
+}
+
+export default Map;
+
+const styles = StyleSheet.create({});
---
+++ screens/PlaceDetails.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+function PlaceDetails() {
+  return (
+    <View>
+      <Text>PlaceDetails</Text>
+    </View>
+  );
+}
+
+export default PlaceDetails;
+
+const styles = StyleSheet.create({});
